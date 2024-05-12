84564c4d02f8c2ce4060ff1707bca92e4a1078e8
Chirag Agarwal <113764729+Chiragarwall@users.noreply.github.com>
2024-05-12 11:20:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,49 +6,29 @@ let hexValRef = document.getElementById("hex-val-ref");
 let rgbValRef = document.getElementById("rgb-val-ref");
 let customAlert = document.getElementById("custom-alert");
 let pickedColorRef = document.getElementById("picked-color-ref");
-let eyeDropper;
 
 window.onload = () => {
-  if ("EyeDropper" in window) {
-    pickColor.classList.remove("hide");
-    eyeDropper = new EyeDropper();
-  } else {
+  if (!("HTMLInputElement" in window && typeof HTMLInputElement.prototype.type === 'string' && "color" in HTMLInputElement.prototype)) {
     error.classList.remove("hide");
-    error.innerText = "Your browser doesn't support Eyedropper API";
+    error.innerText = "Your browser doesn't support ColorPicker API";
     pickColor.classList.add("hide");
     return false;
   }
 };
 
-const colorSelector = async () => {
-  const color = await eyeDropper
-    .open()
-    .then((colorValue) => {
-      error.classList.add("hide");
-      let hexValue = colorValue.sRGBHex;
-      let rgbArr = [];
-      for (let i = 1; i < hexValue.length; i += 2) {
-        rgbArr.push(parseInt(hexValue[i] + hexValue[i + 1], 16));
-        console.log(rgbArr);
-      }
-      let rgbValue = "rgb(" + rgbArr + ")";
-      console.log(hexValue, rgbValue);
-      result.style.display = "grid";
-      hexValRef.value = hexValue;
-      rgbValRef.value = rgbValue;
-      pickedColorRef.style.backgroundColor = hexValue;
-    })
-    .catch((err) => {
-      error.classList.remove("hide");
-      if (err.toString().includes("AbortError")) {
-        error.innerText = "";
-      } else {
-        error.innerText = err;
-      }
-    });
+const colorSelector = () => {
+  let colorValue = pickColor.value;
+  error.classList.add("hide");
+  let hexValue = colorValue;
+  let rgbArr = hexToRgb(hexValue);
+  let rgbValue = "rgb(" + rgbArr.join(",") + ")";
+  result.style.display = "grid";
+  hexValRef.value = hexValue;
+  rgbValRef.value = rgbValue;
+  pickedColorRef.style.backgroundColor = hexValue;
 };
 
-pickColor.addEventListener("click", colorSelector);
+pickColor.addEventListener("input", colorSelector);
 
 fileInput.onchange = () => {
   result.style.display = "none";
@@ -67,3 +47,11 @@ let copy = (textId) => {
     customAlert.style.transform = "scale(0)";
   }, 2000);
 };
+
+// Helper function to convert hex color to RGB array
+function hexToRgb(hex) {
+  let r = parseInt(hex.substring(1, 3), 16);
+  let g = parseInt(hex.substring(3, 5), 16);
+  let b = parseInt(hex.substring(5, 7), 16);
+  return [r, g, b];
+}
